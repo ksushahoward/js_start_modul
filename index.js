c791ab7d88b2fa2c2a49c082c44311d52d8ac55b
ksushahoward <108617945+ksushahoward@users.noreply.github.com>
2022-10-07 00:46:15 +0300

    js

---
+++ index.js
@@ -0,0 +1,119 @@
+// const age = 10;
+// const totalPrice = 200.74;
+// const userName = 'Chelsy';
+// const message = 'Добро пожаловать!';
+// const isOpen = true;
+// const shouldConfirm = false;
+
+// console.log('price', totalPrice);
+
+// const type = typeof isOpen;
+
+// console.log('do');
+
+// alert('qweqwe');
+
+// console.log('posle');
+
+// const shouldRenew = confirm('хотите продлить подписку?');
+// console.log(shouldRenew);
+
+// let quantity = prompt('введите количество товара');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let elementWidth = '50px';
+// elementWidth = Number.parseInt(elementWidth);
+// console.log('elementWidth: ', elementWidth);
+
+// let elementHeight = '200.74px';
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log(elementHeight);
+
+// let salary = 1300.16472;
+// salary = Number(salary.toFixed(3));
+// console.log(salary);
+
+// let quantity = '30';
+// let value = 'эту строку невозможно привести к числу';
+
+// console.log(Number(quantity));
+// console.log(Number(value));
+
+// const base = 5;
+// const power = 5;
+
+// const result = Math.pow(base, power);
+// console.log(base ** power);
+
+// let base = prompt('Давай число');
+// base = Number(base);
+
+// console.log(base);
+
+// let power = prompt('Давай степень');
+// power = Number(power);
+
+// console.log(power);
+
+// const result = base ** power;
+// console.log(result);
+
+// const max = 50;
+// const min = 30;
+
+// const result = Math.random() * (max - min) + min;
+
+// console.log(Math.round(result));
+
+// const message = 'В этой строке 26 символfffffов.';
+
+// console.log(message.length);
+
+// const firstName = 'Chelsy';
+// const lastName = 'Emerald';
+// const fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// const room = 716;
+// const type = 'VIP';
+
+// const welcomeMsg = `Гость ${firstName} ${lastName} поселяется в номер ${room} ${type}
+// `;
+// console.log(welcomeMsg);
+
+// let brand = prompt('brand');
+// brand = brand.toLowerCase();
+
+// console.log(brand);
+// const inDb = 'samsung';
+
+// let brand = 'samSUnG';
+// console.log(brand[4]);
+// console.log(brand.slice(1).toLowerCase());
+
+// brand = brand[0] + brand.slice(1).toLowerCase();
+// console.log(brand);
+
+// const blacklistWord1 = 'спам';
+// const blacklistWord2 = 'распродажа';
+
+// const string1 = 'Привет я принц Абдул, это не спам, предлагаю тебе миллион';
+// const string2 = 'Самая большая РАСПРОДАЖА этой недели, не пропустите!';
+// const string3 = 'Рекламная компания #fatlivesmatter';
+
+// console.log(string1.includes(blacklistWord1));
+// console.log(string1.includes(blacklistWord2));
+
+// console.log(string2.includes(blacklistWord1));
+// console.log(string2.toLowerCase().includes(blacklistWord2));
+
+// console.log(string3.includes(blacklistWord1));
+// console.log(string3.includes(blacklistWord2));
+
+// console.log(10 > 5);
+
+const isEqual = true === 1;
+
+console.log(isEqual);
